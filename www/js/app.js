02934f8ba5d844737e90feea689ab5337814f603
Daniel Zen <daniel@zendigital.com>
2014-06-18 15:14:11 -0400

    v3 Ability to create new tasks.

--- www/js/app.js
+++ www/js/app.js
@@ -1,17 +1,46 @@
 angular.module('todo', ['ionic'])
-  .controller('TodoCtrl', function($scope) {
+  .controller('TodoCtrl', function($scope, $ionicModal) {
     // Initialize tasks
-    $scope.tasks =
-      [
-        {title: "First", completed: true},
-        {title: "Second", completed: false},
-        {title: "Third", completed: false},
-      ];
+    ////////////////////////
+    // tasks starts empty
+    ////////////////////////
+    $scope.tasks = [];
 
-    ////////////////////////
-    // TOGGLE TASK COMPLETED
-    ////////////////////////
     $scope.completionChanged = function(task) {
       task.completed = !task.completed;
     };
+
+    ////////////////////////
+    // Use $ionicModal to put 'taskModal' in $scope
+    ////////////////////////
+    $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+      $scope.taskModal = modal;
+    }, {
+      scope: $scope
+    });
+
+    ////////////////////////
+    // push new task onto tasks array
+    ////////////////////////
+    $scope.createTask = function(task) {
+      task.completed = false;
+      $scope.tasks.push(angular.copy(task));
+      task.title = "";
+      $scope.taskModal.hide();
+    };
+
+    ////////////////////////
+    // show taskModal for newTask
+    ////////////////////////
+    $scope.newTask = function() {
+      $scope.taskModal.show();
+    };
+
+    ////////////////////////
+    // hide taskModal to closeNewTask
+    ////////////////////////
+    $scope.closeNewTask = function() {
+      $scope.taskModal.hide();
+    };
+
   });
