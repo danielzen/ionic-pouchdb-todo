be39a5b4612459a4a278ed39dd4ba6290e91315f
Daniel Zen <daniel@zendigital.com>
2014-06-18 15:16:28 -0400

    v3 Able to persist in local storage with PouchDb

--- www/js/app.js
+++ www/js/app.js
@@ -1,44 +1,86 @@
 angular.module('todo', ['ionic'])
-  .controller('TodoCtrl', function($scope, $ionicModal) {
+  ////////////////////////
+  // Simple PouchDB synchronization factory
+  ////////////////////////
+  .factory('todoDb', function() {
+    var db = new PouchDB('todos');
+    return db;
+  })
+  .controller('TodoCtrl', function($scope, $ionicModal, todoDb) { // inject todoDb factory
     // Initialize tasks
-    ////////////////////////
-    // tasks starts empty
-    ////////////////////////
     $scope.tasks = [];
 
     $scope.completionChanged = function(task) {
       task.completed = !task.completed;
+      this.update(task);
     };
 
     ////////////////////////
-    // Use $ionicModal to put 'taskModal' in $scope
+    // http://pouchdb.com/api.html#changes
+    // list of changes to docs in todoDb
+    // modify ng-model accordingly
     ////////////////////////
+    todoDb.changes({
+      live: true,
+      onChange: function (change) {
+        if (!change.deleted) { // IF THE CHANGE IS A DELETE, IGNORE
+          // GET THE doc (a task) from the change.id
+          todoDb.get(change.id, function(err, doc) {
+            if (err) console.log(err); ////////////
+            $scope.$apply(function() { // UPDATE //
+              // INEFFICIENTLY FIND task THAT HAS CHANGED
+              for (var i = 0; i < $scope.tasks.length; i++) {
+                if ($scope.tasks[i]._id === doc._id) {
+                  // REPLACE THE TASK WITH FETCHED doc
+                  $scope.tasks[i] = doc;
+                  return;
+                } //////////////////////////////////////////
+              }   // IF UNIQUE doc._id NOT FOUND ADD task //
+              $scope.tasks.push(doc);
+            });
+          })
+        }
+      }
+    });
+
+    ////////////////////////
+    // UPDATE task IN POUCHDB
+    ////////////////////////
+    $scope.update = function (task) {
+      todoDb.get(task._id, function (err, doc) {
+        if (err) {
+          console.log(err);
+        } else {
+          todoDb.put(angular.copy(task), doc._rev, function (err, res) {
+            if (err) console.log(err);
+          });
+        }
+      });
+    };
+
+    // Create our modal
     $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
       $scope.taskModal = modal;
     }, {
       scope: $scope
     });
 
-    ////////////////////////
-    // push new task onto tasks array
-    ////////////////////////
     $scope.createTask = function(task) {
       task.completed = false;
-      $scope.tasks.push(angular.copy(task));
-      task.title = "";
+      ////////////////////////
+      // CREATE task IN POUCHDB
+      ////////////////////////
+      todoDb.post(angular.copy(task), function(err, res) {
+        if (err) console.log(err)
+        task.title = "";
+      });
       $scope.taskModal.hide();
     };
 
-    ////////////////////////
-    // show taskModal for newTask
-    ////////////////////////
     $scope.newTask = function() {
       $scope.taskModal.show();
     };
 
-    ////////////////////////
-    // hide taskModal to closeNewTask
-    ////////////////////////
     $scope.closeNewTask = function() {
       $scope.taskModal.hide();
     };
